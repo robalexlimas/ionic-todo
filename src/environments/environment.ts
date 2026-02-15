4e75e58dd2fa8fadcee6d0b51d212a5d4ed34605
Robert Limas Sierra <robert.limassierra@polito.it>
2026-02-15 18:16:13 -0500

    complete technical test - todos & categories CRUD, feature flags, performance optimizations, iOS Cordova setup and delivery documentation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'LOCAL_ONLY',
-    authDomain: 'LOCAL_ONLY',
-    projectId: 'LOCAL_ONLY',
-    appId: 'LOCAL_ONLY',
-  },
+    apiKey: "AIzaSyBAOt6WwG65d9jgbHGiwzAc4gTYC4O69U8",
+    authDomain: "todo-list-8f0a5.firebaseapp.com",
+    projectId: "todo-list-8f0a5",
+    storageBucket: "todo-list-8f0a5.firebasestorage.app",
+    messagingSenderId: "569563151856",
+    appId: "1:569563151856:web:803e5e1ef48391c17eb135",
+    measurementId: "G-YV0RKQVZWD"
+  }
 };
 
 /*
